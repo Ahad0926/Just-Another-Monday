ec0fb201c68f353b8f9ef9c3f708098e26ae8a0e
Ahad0926 <ahadabdu0926@gmail.com>
2025-02-22 15:06:10 -0500

    Added Scene transition script

--- Scripts/scene_transition.gd
+++ Scripts/scene_transition.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var target_scene: String = ""  # Scene file path to transition to
 var entered = false
 
 func _on_body_entered(body: CharacterBody2D) -> void:
@@ -9,6 +10,5 @@ func _on_body_exited(body: Node2D) -> void:
 	entered = false
 
 func _process(delta: float) -> void:
-	if entered == true:
-		print("Player exits the apartment!")
-		get_tree().change_scene_to_file("res://Scenes/hallway.tscn")  # Call scene loader in Main
+	if entered == true and target_scene != "":
+		get_tree().change_scene_to_file(target_scene)
