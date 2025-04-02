7c649455522c96f3744d8977f09d4106f0cfef80
Ahad0926 <ahadabdu0926@gmail.com>
2025-04-02 06:51:40 -0400

    Added title screen

--- Scenes/Title/title.gd
+++ Scenes/Title/title.gd
@@ -1,9 +1,23 @@
-extends Node2D
-
-@onready var player: Player = $player
-@onready var canvas_modulate = $CanvasModulate
-@onready var time_canvas_layer = $Camera2D/CanvasLayer
-
-# Called when the node enters the scene tree for the first time.
+extends BaseScene
+@onready var button = $StartButton
 func _ready() -> void:
-	scene_manager.change_scene(self, "Apartment/apartment")
+	super()
+	canvas_modulate.freeze()
+	time_canvas_layer.hide()
+
+	# Optional: freeze the player so they're not active in memory
+	if player:
+		player.freeze()
+
+	# Connect StartButton signal
+	button.pressed.connect(_on_start_pressed)
+
+func _on_start_pressed() -> void:
+	if canvas_modulate:
+		canvas_modulate.unfreeze()
+	if time_canvas_layer:
+		time_canvas_layer.show()
+	if player:
+		player.unfreeze()
+
+	await scene_manager.change_scene(self, "Apartment/apartment")
