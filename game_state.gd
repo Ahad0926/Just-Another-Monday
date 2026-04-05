6412b2ee980b7a6f9c87e320a57f91e34c4e3461
Ahad0926 <ahadabdu0926@gmail.com>
2026-04-05 14:34:05 -0400

    Updated to Godot 4.4

--- game_state.gd
+++ game_state.gd
@@ -5,8 +5,8 @@ var current_day: int
 var current_hour: int
 var current_minute: int
 
-var game_start: bool = false
-var emails_done: bool = true
+var game_start: bool = true
+var emails_done: bool = false
 var peed: bool = false
 var dishes: bool = false
 var email_talked: bool = false
